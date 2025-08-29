ad361e66a8bcdea7461efa5f1f59d791a3413534
greenhc502 <greenhc502@gmail.com>
2025-08-29 16:38:02 +0900

    카카오페이 연동

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,12 +5,12 @@ import ProfileImg from './ProfileImg.vue';
 import { useAuthenticationStore } from '@/stores/authentication';
 import { signOut } from '@/services/userService';
 import { reactive, watch } from 'vue';
-import { getKeywordList, getFeedList } from '@/services/feedService';
+import { getKeywordList } from '@/services/feedService';
 import { useFeedStore } from '@/stores/feed';
 import { useRoute } from 'vue-router';
+import { postPayReady } from '@/services/payService';
 
 const route = useRoute();
-
 const feedStore = useFeedStore();
 const authenticationStore = useAuthenticationStore();
 
@@ -18,11 +18,6 @@ const state = reactive({
     search: '',
     searchList: []
 })
-
-watch(()=> route.fullPath, ()=>{
-state.search='';
-});
-
 
 const doSignOut = async () => {    
     const res = await signOut();
@@ -57,14 +52,25 @@ const getSearchKeyword = async () => {
 }
 
 const getFeedData = async () => {
-
-
     feedStore.setKeyword(state.search);
     feedStore.clearList();
     feedStore.setPage(1);
     feedStore.setReLoading(true);
-
 }
+
+const buy = async () => {
+    const res = await postPayReady();
+
+    if(res.status === 200) {
+        const data = res.data.result;
+
+        window.location.href = data.next_redirect_pc_url;
+    }
+}
+
+watch(() => route.fullPath, () => {    
+    state.search = '';    
+});
 </script>
 
 <template>    
@@ -72,7 +78,7 @@ const getFeedData = async () => {
     <div id="globalConst">
         <div class="d-flex flex-column flex-md-row align-items-center">
             <div class="d-inline-flex flex-grow-1 flex-shrink-0">
-                <router-link to="/" ><img :src="logo" class="h24 w24" /></router-link>
+                <router-link to="/"><img :src="logo" class="h24 w24" /></router-link>
             </div>
             <div class="d-inline-flex flex-grow-1 flex-shrink-0">
                 <b-form-input list="search-list-id" @input="onTyping" @keyup.enter="getFeedData" v-model="state.search"/>
@@ -101,7 +107,7 @@ const getFeedData = async () => {
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navDropdownMenuLink">
                             <li>
-                                <router-link :to="`/profile/${authenticationStore.state.signedUser.userId}`" @click="getFeedData('')">                      
+                                <router-link :to="`/profile/${authenticationStore.state.signedUser.userId}`">
                                     <span class="dropdown-item">
                                         <span>
                                             <svg aria-label="프로필"  class="_8-yf5 " color="#262626" fill="#262626" height="16" role="img" viewBox="0 0 24 24" width="16">
@@ -112,7 +118,17 @@ const getFeedData = async () => {
                                         <span>프로필</span>
                                     </span>
                                 </router-link>
-                            </li>                 
+                            </li>          
+                            <li>
+                                <span class="dropdown-item pointer" @click="buy">
+                                    <span>
+                                        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 114.58" height="16" width="16">
+                                            <title>product</title><path d="M118.13,9.54a3.25,3.25,0,0,1,2.2.41,3.28,3.28,0,0,1,2,3l.57,78.83a3.29,3.29,0,0,1-1.59,3L89.12,113.93a3.29,3.29,0,0,1-2,.65,3.07,3.07,0,0,1-.53,0L3.11,105.25A3.28,3.28,0,0,1,0,102V21.78H0A3.28,3.28,0,0,1,2,18.7L43.89.27h0A3.19,3.19,0,0,1,45.63,0l72.5,9.51Zm-37.26,1.7-24.67,14,30.38,3.88,22.5-14.18-28.21-3.7Zm-29,20L50.75,64.62,38.23,56.09,25.72,63.17l2.53-34.91L6.55,25.49V99.05l77.33,8.6V35.36l-32-4.09Zm-19.7-9.09L56.12,8,45.7,6.62,15.24,20l16.95,2.17ZM90.44,34.41v71.12l25.9-15.44-.52-71.68-25.38,16Z"/>
+                                        </svg>
+                                    </span>&nbsp;
+                                    <span>구매</span>
+                                </span>
+                            </li>       
                             <li><hr class="dropdown-divider"></hr></li>
                             <li><span class="dropdown-item pointer" @click="doSignOut">Sign out</span></li>
                         </ul>
